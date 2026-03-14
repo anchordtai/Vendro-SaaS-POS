75db681bdcacf3ba9c0188b49b5cbd971cb155dc
anchordtai@gmail.com <anchordtai@gmail.com>
2026-03-14 19:12:33 +0100

    fix nextjs config for netlify

--- next.config.js
+++ next.config.js
@@ -4,6 +4,29 @@ const nextConfig = {
   images: {
     domains: ['localhost', 'images.unsplash.com'],
   },
+  experimental: {
+    optimizePackageImports: true,
+  optimizeCss: true,
+  optimizeServerReact: true,
+  esmExternals: true
+  },
+  webpack: (config, { buildId, dev, isServer, webpack }) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        '@': './src',
+      '@/lib': './src/lib',
+        '@/components': './src/components'
+      }
+    };
+    return config;
+  },
+  transpilePackages: ['tailwindcss'],
+  compiler: {
+    removeConsole: {
+      exclude: ['error', 'warn']
+    }
+  }
 }
 
 module.exports = nextConfig
